bd945993b67d6cbf721eef583dd26a098195b721
rm <rm@db.de>
2018-09-06 11:47:02 +0200

    new column description

--- worblehat-acceptancetests/src/main/java/de/codecentric/worblehat/acceptancetests/step/business/DemoBookFactory.java
+++ worblehat-acceptancetests/src/main/java/de/codecentric/worblehat/acceptancetests/step/business/DemoBookFactory.java
@@ -7,7 +7,7 @@ public class DemoBookFactory {
     private Book book;
 
     private DemoBookFactory(){
-        this.book = new Book("A book title", "A book author", "1", "1234567890", 2013);
+        this.book = new Book("A book title", "A book author", "1", "1234567890", 2013, "Test Description");
     }
 
     public static DemoBookFactory createDemoBook(){
@@ -44,6 +44,11 @@ public class DemoBookFactory {
         return this;
     }
 
+    public DemoBookFactory withDescription(String description){
+        this.book.setDescription(description);
+        return this;
+    }
+    
     public Book build(){
         return book;
     }
